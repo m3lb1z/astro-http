deed1ac1657ffd4289cfc6fc844e8c2384785fef
Elvis M. <u201112959@upc.edu.pe>
2025-06-22 08:28:28 -0500

    Refactor API routes and remove unused code
    - Removed the API route for fetching posts by slug and the associated GET, POST, and DELETE methods.
    - Deleted the API route for fetching all posts and filtering by slug.
    - Removed the API route for managing post likes, including GET and PUT methods.
    - Updated the helper function to remove the unused database import and the getPostById function.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,24 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
-import db from "@astrojs/db";
+// import db from "@astrojs/db";
 
 import vue from "@astrojs/vue";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-http-e1k.pages.dev",
-  integrations: [mdx(), sitemap(), db(), vue()],
-  adapter: cloudflare(),
+  // integrations: [mdx(), sitemap(), db(), vue()],
+  integrations: [mdx(), sitemap(), vue()],
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
+  },
+  // adapter: cloudflare(),
+  adapter: node({
+    mode: "standalone",
+  }),
 });
--- src/actions/helpers/index.ts
+++ src/actions/helpers/index.ts
@@ -1,10 +1,4 @@
 import prisma from "@/db";
-import { db, Posts, eq } from "astro:db";
-
-export async function getPostById(postId: string) {
-  const [post] = await db.select().from(Posts).where(eq(Posts.id, postId));
-  return post;
-}
 
 export async function getPostIdLikes(postId: string) {
   const post = await prisma.post.findFirst({
